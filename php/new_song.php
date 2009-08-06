0faa49acde9fcee2856b206a95b6f41bc200e4cb
Nik Wakelin <nik@codetocustomer.com>
2009-08-06 15:24:06 +1200

    PHP Skeleton and instructions

---
+++ php/new_song.php
@@ -0,0 +1,93 @@
+<?php
+
+  function connect_to_database() {
+    
+    if (!($m = mysql_connect("localhost", "username", "password"))) die("Couldn't connect to mysql, oh noes!");
+    if (!mysql_select_db("database_name", $m)) die("Couldn't select that db, oh noes!");
+  
+    return true;
+    
+  }
+  
+  function create_song($attributes) {
+    
+    // COMPLETE ME
+    
+    // THIS WILL HELP YOU: http://nz.php.net/manual/en/function.mysql-query.php
+    // QUERY HINT: insert into `users` set name = '$something';    
+    
+    return false;
+    
+  }
+  
+  connect_to_database();
+  
+  if ($_POST["add"]) {
+    
+    if (add_song($_POST)) {
+      
+      header("Location: songs.php");
+      exit(); 
+      
+    } else {
+      
+      // you'll need to display some error messages
+      // remember you can embed php into html using the start/stop tags
+      
+    }
+    
+    
+  }
+  
+  
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<title>Non-Sucky Lyrics Site &mdash; Make a New Song</title>
+
+</head>
+
+<body>
+
+  <h1>Non Sucky Lyrics Site</h1>
+  
+  <p>The time is: <?php echo(date("%H:%i:%s")) ?></p>
+  
+  <form action="new_song.php">
+    
+    <p>
+      <label>Artist</label><br />
+      <input type="text" name="artist" size="30" />
+    </p>
+
+    <p>
+      <label>Album</label><br />
+      <input type="text" name="album" size="30" />
+    </p>
+
+    <p>
+      <label>Title</label><br />
+      <input type="text" name="title" size="30" />
+    </p>
+    
+    <p>
+      <label>Lyrics</label><br />
+      <textarea name="lyrics" rows="50" cols="30" />
+    </p>
+    
+    <p>
+      <input type="submit" name="add" value="Add Song" />
+    </p>
+    
+  
+  </form>
+  
+
+</body>
+</html>
+
+
+
---
+++ php/songs.php
@@ -0,0 +1,15 @@
+<?php
+
+  // I need to connect to the database somehow.....?
+  
+  // Then I'm going to need to get all the songs out of the database.... 
+  // perhaps with a select query?
+
+
+?>
+
+<!-- Well, this isn't going to do much. I probably need an HTML skeleton, then perhaps a <table> tag -->
+
+<!-- Maybe a listing of all the songs -->
+
+<!-- A link to add a new song might be nice too -->
